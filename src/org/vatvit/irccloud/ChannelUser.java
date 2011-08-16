b2ef948d15195b5bf8af86ba109aa52d72e62856
Jaakko Lukkari <jaakko.lukkari@gmail.com>
2011-08-16 22:33:48 +0300

    Removed unnecessary files

--- src/org/vatvit/irccloud/ChannelUser.java
+++
@@ -1,9 +0,0 @@
-package org.vatvit.irccloud;
-
-import org.json.JSONObject;
-
-public class ChannelUser {
-	public ChannelUser(Connection conn, JSONObject object) {
-		
-	}
-}
--- src/org/vatvit/irccloud/User.java
+++
@@ -1,9 +0,0 @@
-package org.vatvit.irccloud;
-
-import org.json.JSONObject;
-
-public class User {
-	public User(Connection conn, JSONObject object) {
-		
-	}
-}
